39e0a1c4adcbd70a97b3ba1d81b89a12b6b2d638
fakharrana <fakharayub98@gmail.com>
2020-11-27 18:45:10 +0500

    Task 3

--- App.js
+++ App.js
@@ -1,26 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      originalPrice: null,
+      discountPercentage: null,
+      youSaved: null,
+      finalPrice: null,
+    };
+  }
 
-    <View style={{ flexDirection:"row" , justifyContent: "space-between"}}> 
-      <TextInput
-      style={{ height: 40 ,marginRight:20 , width:150,  textAlign:"center", borderColor: 'gray', borderWidth: 1 }}
-      placeholder="Original Price"
-      keyboardType="number-pad"
-    />
-      <TextInput
-      style={{ height: 40,width:150, textAlign:"center", borderColor: 'gray', borderWidth: 1 }}
-      placeholder="Discount Percentage"
-    />
-      <StatusBar style="auto" />
+  update = () => {
+    const youSaved =
+      (this.state.originalPrice * this.state.discountPercentage) / 100;
+    const finalPrice = this.state.originalPrice - youSaved;
+
+    if (
+      this.state.originalPrice != null &&
+      this.state.discountPercentage != null
+    ) {
+      this.setState({
+        youSaved: youSaved,
+        finalPrice: finalPrice,
+        originalPrice: null,
+        discountPercentage: null,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TextInput
+            style={{
+              height: 40,
+              marginRight: 20,
+              width: 150,
+              textAlign: 'center',
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={this.state.originalPrice}
+            placeholder="Original Price"
+            keyboardType="number-pad"
+            onChangeText={(originalPrice) => this.setState({ originalPrice })}
+            onEndEditing={this.update}
+          />
+          <TextInput
+            style={{
+              height: 40,
+              width: 150,
+              textAlign: 'center',
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            value={this.state.discountPercentage}
+            placeholder="Discount Percentage"
+            keyboardType="number-pad"
+            onChangeText={(discountPercentage) =>
+              this.setState({ discountPercentage })
+            }
+            onEndEditing={this.update}
+          />
+        </View>
+
+        <View style={{ marginTop: 8 }}>
+          <Text>You Saved : {this.state.youSaved}</Text>
+          <Text>Final Price : {this.state.finalPrice}</Text>
+        </View>
       </View>
-
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
