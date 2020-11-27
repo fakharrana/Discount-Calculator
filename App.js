5cdde806d7b064e72947f5582c21560bdb2c71e5
fakharrana <fakharayub98@gmail.com>
2020-11-27 16:39:54 +0500

    Task 2

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={{ flexDirection:"row" , justifyContent: "space-between"}}> 
+      <TextInput
+      style={{ height: 40 ,marginRight:20 , width:150,  textAlign:"center", borderColor: 'gray', borderWidth: 1 }}
+      placeholder="Original Price"
+      keyboardType="number-pad"
+    />
+      <TextInput
+      style={{ height: 40,width:150, textAlign:"center", borderColor: 'gray', borderWidth: 1 }}
+      placeholder="Discount Percentage"
+    />
       <StatusBar style="auto" />
+      </View>
+
     </View>
   );
 }
